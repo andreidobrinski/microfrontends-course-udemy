00cb486ce92b40200a0433ed3421ec2671b8ff48
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-04 20:09:12 -0500

    refactor subproject to work for dev and prod environment with container

--- ecomm/products/src/bootstrap.js
+++ ecomm/products/src/bootstrap.js
@@ -1,10 +1,32 @@
 import faker from 'faker';
 
-let products = '';
+const mount = (el) => {
+  let products = '';
 
-for (let i = 0; i < 3; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+  for (let i = 0; i < 3; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+
+  el.innerHTML = products;
+};
+
+// context/situation #1
+// running in dev env in isolation
+// using local index.html file with an id of dev-products
+// render app into that element
+if (process.env.NODE_ENV === 'development') {
+  // ideally using a unique ID that a parent container app wouldn't have
+  const el = document.querySelector('#dev-products');
+
+  if (el) {
+    // probably running in isolation
+    mount(el);
+  }
 }
 
-document.querySelector('#dev-products').innerHTML = products;
+// context/situation #2
+// running in development or production in container app
+// no guarantee that element with 'dev-products' id exists
+// don't immediately render the app
+export { mount };
