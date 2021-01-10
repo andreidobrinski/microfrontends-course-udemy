4517f5403e85e062397752e3c7baef5dc9127dc4
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-10 18:00:25 -0500

    update boilerplate for vue

--- mfp/packages/dashboard/src/bootstrap.js
+++ mfp/packages/dashboard/src/bootstrap.js
@@ -1,40 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createMemoryHistory, createBrowserHistory } from 'history';
-import App from './App';
+import { createApp } from 'vue';
+import Dashboard from './components/Dashboard.vue';
 
 // mount function to start the app
-const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
-  const history = defaultHistory || createMemoryHistory({
-    initialEntries: [initialPath]
-  });
+const mount = (el) => {
+  const app = createApp(Dashboard);
 
-  if (onNavigate) {
-    history.listen(onNavigate);
-  }
-
-  ReactDOM.render(
-    <App history={history} onSignIn={onSignIn} />,
-    el
-  );
-
-  return {
-    onParentNavigate({ pathname: nextPathname }) {
-      const { pathname } = history.location;
-
-      if (pathname !== nextPathname) {
-        history.push(nextPathname);
-      }
-    }
-  };
+  app.mount(el);
 };
 
 // if in dev env and in isolation, mount immediately
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_auth-dev-root');
+  const devRoot = document.querySelector('#_dashboard-dev-root');
 
   if (devRoot) {
-    mount(devRoot, { defaultHistory: createBrowserHistory() });
+    mount(devRoot);
   }
 }
 
