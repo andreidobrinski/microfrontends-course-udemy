414ed8d0c584b7ae7813760afdd45705cb500775
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-10 17:46:43 -0500

    copy boilerplate to dashboard app

---
+++ mfp/packages/dashboard/src/bootstrap.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+import App from './App';
+
+// mount function to start the app
+const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
+  const history = defaultHistory || createMemoryHistory({
+    initialEntries: [initialPath]
+  });
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(
+    <App history={history} onSignIn={onSignIn} />,
+    el
+  );
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    }
+  };
+};
+
+// if in dev env and in isolation, mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_auth-dev-root');
+
+  if (devRoot) {
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+// if running through a container, export mount
+export { mount };
